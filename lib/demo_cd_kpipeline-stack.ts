7fce9f00395deb640fe88a68efba241933d1f4ad
amafru <fruamabuh@gmail.com>
2023-05-20 18:07:45 +0100

    added deployment stages and a manual approval step

--- lib/demo_cd_kpipeline-stack.ts
+++ lib/demo_cd_kpipeline-stack.ts
@@ -2,6 +2,11 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
+//Import the added stages from the relevant file
+import { PipelineAppStage } from './demopipelinestages-app';
+//Add in a manual approval step for e.g. deploying to Prod if build completes successfully in test env
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+
 export class DemoCdKpipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -18,5 +23,15 @@ export class DemoCdKpipelineStack extends cdk.Stack {
         ],
       })
     })
+
+    const testingStage = democicdpipeline.addStage(new PipelineAppStage(this, 'test', {
+      env: { account: '769065143225', region: 'eu-west-1' }
+    }));
+
+    testingStage.addPost(new ManualApprovalStep('approval'));
+
+    const prodStage = democicdpipeline.addStage(new PipelineAppStage(this, 'prod', {
+      env: { account: '769065143225', region: 'eu-west-1' }
+    }));
   }
 }
